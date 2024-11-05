7f534d73a55ac2ea038cf452cc73dd74f4299e61
oluwadamilareoyebode <oluwadamilareoyebode@gmail.com>
2024-11-05 22:25:06 +0000

    WhatsUp Commit

---
+++ firebase_options.dart
@@ -0,0 +1,11 @@
+// File: firebase_options.dart
+import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
+
+class DefaultFirebaseOptions {
+  static FirebaseOptions get current => const FirebaseOptions(
+        apiKey: 'YOUR_API_KEY',
+        appId: 'YOUR_APP_ID',
+        messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
+        projectId: 'YOUR_PROJECT_ID',
+      );
+}
